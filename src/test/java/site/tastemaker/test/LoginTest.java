37f09924c9caef0c6ca5ed7f142aa3ea1a39c106
Yaroslav <yaroslavcher@gmail.com>
2024-04-23 23:12:57 +0300

    first commit

---
+++ src/test/java/site/tastemaker/test/LoginTest.java
@@ -0,0 +1,10 @@
+package site.tastemaker;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class LoginTest {
+    LoginPage loginPage;
+    
+}
